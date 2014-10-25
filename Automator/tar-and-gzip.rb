6b522ab5225f4da27e8efdbca5f731f1fdb8a064
Stephen Benner <sbenner9@gmail.com>
2014-10-24 21:41:32 -0700

    QUICK COMMIT! This was done automatically by a script, and should be reviewed.

--- Automator/tar-and-gzip.rb
+++ Automator/tar-and-gzip.rb
@@ -1,3 +1,3 @@
 Dir.chdir File.dirname(ARGV[0])
-`tar -czf #{File.basename ARGV[0]}.tgz #{File.basename ARGV[0]}`
+`tar -czf #{File.basename(ARGV[0]).gsub(/ /, '-')}.tgz '#{File.basename ARGV[0]}'`
 puts File.basename(ARGV[0])
