6ba9bcf4996bd94f83f46243c6abf118172d6eb8
Stephen Benner <sbenner9@gmail.com>
2014-12-22 23:26:40 -0800

    updated a script

--- Automator/tar-and-gzip.rb
+++ Automator/tar-and-gzip.rb
@@ -1,3 +1,8 @@
+# NOTE: THIS SCRIPT IS DESIGNED TO BE RUN AS PART OF AN AUTOMATOR WORKFLOW
+#
+# Arguments:
+#   0 - Path of the file or directory to compress and archive
+#
 Dir.chdir File.dirname(ARGV[0])
 `tar -czf #{File.basename(ARGV[0]).gsub(/ /, '-')}.tgz '#{File.basename ARGV[0]}'`
 puts File.basename(ARGV[0])
